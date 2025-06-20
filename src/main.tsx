d4452bde045f515d79123aba1bcb697417e72a02
dheerajttt <dheeraj@thethinktech.com>
2025-06-21 00:17:40 +0530

    added certificate layout

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,7 @@ import LoginAdmin from "./pages/LoginAdmin.tsx";
 import Dashboard from "./pages/Dashboard.tsx";
 import ClientDashboard from "./components/client/ClientDashboard.tsx";
 import CoursePlay from "./components/client/CoursePlay.tsx";
+import Certificate from "./components/Certificate.tsx";
 // import Quiz from './components/client/Quiz.tsx';
 
 const router = createBrowserRouter(
@@ -21,6 +22,7 @@ const router = createBrowserRouter(
       <Route path="/admindashboard" element={<Dashboard />} />
       <Route path="/client" element={<ClientDashboard />} />
       <Route path="/play/:id" element={<CoursePlay />} />
+      <Route path="/certificates" element={<Certificate />} />
       {/* <Route path='/quiz' element={<Quiz />} /> */}
     </Route>
   )
