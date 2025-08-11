b53c45f6a4b0366d98df53bd72e95aa4a9afe394
dheerajttt <dheeraj@thethinktech.com>
2025-08-11 20:56:52 +0530

    create admin component fix

--- src/components/Admin/AdminCreateAdmin.tsx
+++ src/components/Admin/AdminCreateAdmin.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import {
-  useAdminCreateClient,
-  type CreateClientUserDto,
-} from "../../hooks/useAdminCreateClient";
+// import { useAdminCreateClient, type CreateClientUserDto } from '../../hooks/useAdminCreateClient';
 import useNotification from "../../hooks/useNotification";
+import {
+  useCreateAdminUser,
+  type CreateAdminUserDto,
+} from "../../hooks/useCreateAdminUser";
 
 export const AdminCreateAdmin: React.FC = () => {
   const {
@@ -13,27 +14,27 @@ export const AdminCreateAdmin: React.FC = () => {
     reset,
     watch,
     formState: { errors },
-  } = useForm<CreateClientUserDto & { confirmPassword: string }>();
-  const { createClient, loading, error, success } = useAdminCreateClient();
+  } = useForm<CreateAdminUserDto & { confirmPassword: string }>();
+
+  const { createUser, loading } = useCreateAdminUser();
   const { triggerNotification, NotificationComponent } = useNotification();
 
   const onSubmit = async (
-    data: CreateClientUserDto & { confirmPassword: string }
+    data: CreateAdminUserDto & { confirmPassword: string }
   ) => {
     const { name, email, password } = data;
     try {
-      await createClient({ name, email, password });
-      if (success)
-        triggerNotification({
-          type: "success",
-          message: "Client created successfully",
-          duration: 3000,
-        });
+      await createUser({ name, email, password });
+      triggerNotification({
+        type: "success",
+        message: "Admin user created",
+        duration: 3000,
+      });
       reset();
-    } catch (error) {
+    } catch {
       triggerNotification({
         type: "error",
-        message: "Failed to create client",
+        message: "Failed to create admin",
         duration: 3000,
       });
     }
