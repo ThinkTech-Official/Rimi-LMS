1f589d7330b93f49408c891d756e3d08c6093101
ThinkTech <tech@thethinktech.com>
2025-09-08 20:19:00 +0530

    passing static 1 in quiz question number

--- src/components/CreateTest.tsx
+++ src/components/CreateTest.tsx
@@ -215,7 +215,8 @@ const CreateTest: React.FC = () => {
       duration: Number(data.duration),
       startTime: Number(data.startTime),
       passingMarks: Number(data.passingMarks),
-      quizQuestionNumber: Number(data.quizQuestionNumber),
+      // quizQuestionNumber: Number(data.quizQuestionNumber) ,
+      quizQuestionNumber: 1 ,
       questions: questions.map((q) => ({
         text: q.text.trim(),
         options: q.options.map((o) => ({
