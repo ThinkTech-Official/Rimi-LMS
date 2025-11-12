773403d01aec90fd4caf090ffd1ebf3ffd67c3e1
Hubtec Digital India <dheeraj@thethinktech.com>
2025-11-12 18:45:48 +0530

    language on edit category modal

--- src/components/client/ClientHeader.tsx
+++ src/components/client/ClientHeader.tsx
@@ -181,8 +181,8 @@ const ClientHeader: React.FC = () => {
           />
         </button>
         {isProfileMenuOpen && (
-          <div className="absolute mt-4 ml-1 w-28 2xl:w-32 rounded-sm shadow-lg bg-white border border-[#E9EEF1] z-10">
-            <ul className="py-1 text-sm 2xl:text-lg text-gray-700">
+          <div className="absolute mt-4 ml-1 w-36 2xl:w-40 rounded-sm shadow-lg bg-white border border-[#E9EEF1] z-10">
+            <ul className="py-1 text-sm 2xl:text-base text-gray-700">
               <li>
                 <button
                   className="w-full text-left px-4 py-2 hover:bg-primary hover:text-white cursor-pointer flex gap-2 items-center"
@@ -198,7 +198,7 @@ const ClientHeader: React.FC = () => {
                   disabled={logOutLoading}
                   className="w-full text-left px-4 py-2 hover:bg-primary hover:text-white cursor-pointer flex gap-2 items-center"
                 >
-                  <IoIosLogOut className="h-4 w-4 2xl:w-5 2xl:h-5" /> Logout
+                  <IoIosLogOut className="h-4 w-4 2xl:w-5 2xl:h-5" /> {t("Logout")}
                 </button>
               </li>
             </ul>
